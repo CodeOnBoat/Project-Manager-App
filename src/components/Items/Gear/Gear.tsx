97fa7629410a4257c7816d6c64c7679be9eb0473
1998KB <klajdibeqiraj88@gmail.com>
2023-05-03 02:18:07 +0200

    loading

---
+++ src/components/Items/Gear/Gear.tsx
@@ -0,0 +1,180 @@
+import React from "react";
+import "./Gear.css";
+import aiLogo from "../../../data/images/aiLogo.png";
+
+export const Gear = () => {
+  return (
+    <div className="loading-contanier">
+      <div className="container">
+        <div className="h1Container">
+          <div className="cube h1 w1 l1">
+            <div className="face top"></div>
+            <div className="face left1"></div>
+            <div className="face right1"></div>
+          </div>
+
+          <div className="cube h1 w1 l2">
+            <div className="face top"></div>
+            <div className="face left1"></div>
+            <div className="face right1"></div>
+          </div>
+
+          <div className="cube h1 w1 l3">
+            <div className="face top"></div>
+            <div className="face left1"></div>
+            <div className="face right1"></div>
+          </div>
+
+          <div className="cube h1 w2 l1">
+            <div className="face top"></div>
+            <div className="face left1"></div>
+            <div className="face right1"></div>
+          </div>
+
+          <div className="cube h1 w2 l2">
+            <div className="face top"></div>
+            <div className="face left1"></div>
+            <div className="face right1"></div>
+          </div>
+
+          <div className="cube h1 w2 l3">
+            <div className="face top"></div>
+            <div className="face left1"></div>
+            <div className="face right1"></div>
+          </div>
+
+          <div className="cube h1 w3 l1">
+            <div className="face top"></div>
+            <div className="face left1"></div>
+            <div className="face right1"></div>
+          </div>
+
+          <div className="cube h1 w3 l2">
+            <div className="face top"></div>
+            <div className="face left1"></div>
+            <div className="face right1"></div>
+          </div>
+
+          <div className="cube h1 w3 l3">
+            <div className="face top"></div>
+            <div className="face left1"></div>
+            <div className="face right1"></div>
+          </div>
+        </div>
+
+        <div className="h2Container">
+          <div className="cube h2 w1 l1">
+            <div className="face top"></div>
+            <div className="face left1"></div>
+            <div className="face right1"></div>
+          </div>
+
+          <div className="cube h2 w1 l2">
+            <div className="face top"></div>
+            <div className="face left1"></div>
+            <div className="face right1"></div>
+          </div>
+
+          <div className="cube h2 w1 l3">
+            <div className="face top"></div>
+            <div className="face left1"></div>
+            <div className="face right1"></div>
+          </div>
+
+          <div className="cube h2 w2 l1">
+            <div className="face top"></div>
+            <div className="face left1"></div>
+            <div className="face right1"></div>
+          </div>
+
+          <div className="cube h2 w2 l2">
+            <div className="face top"></div>
+            <div className="face left1"></div>
+            <div className="face right1"></div>
+          </div>
+
+          <div className="cube h2 w2 l3">
+            <div className="face top"></div>
+            <div className="face left1"></div>
+            <div className="face right1"></div>
+          </div>
+
+          <div className="cube h2 w3 l1">
+            <div className="face top"></div>
+            <div className="face left1"></div>
+            <div className="face right1"></div>
+          </div>
+
+          <div className="cube h2 w3 l2">
+            <div className="face top"></div>
+            <div className="face left1"></div>
+            <div className="face right1"></div>
+          </div>
+
+          <div className="cube h2 w3 l3">
+            <div className="face top"></div>
+            <div className="face left1"></div>
+            <div className="face right1"></div>
+          </div>
+        </div>
+
+        <div className="h3Container">
+          <div className="cube h3 w1 l1">
+            <div className="face top"></div>
+            <div className="face left1"></div>
+            <div className="face right1"></div>
+          </div>
+
+          <div className="cube h3 w1 l2">
+            <div className="face top"></div>
+            <div className="face left1"></div>
+            <div className="face right1"></div>
+          </div>
+
+          <div className="cube h3 w1 l3">
+            <div className="face top"></div>
+            <div className="face left1"></div>
+            <div className="face right1"></div>
+          </div>
+
+          <div className="cube h3 w2 l1">
+            <div className="face top"></div>
+            <div className="face left1"></div>
+            <div className="face right1"></div>
+          </div>
+
+          <div className="cube h3 w2 l2">
+            <div className="face top"></div>
+            <div className="face left1"></div>
+            <div className="face right1"></div>
+          </div>
+
+          <div className="cube h3 w2 l3">
+            <div className="face top"></div>
+            <div className="face left1"></div>
+            <div className="face right1"></div>
+          </div>
+
+          <div className="cube h3 w3 l1">
+            <div className="face top"></div>
+            <div className="face left1"></div>
+            <div className="face right1"></div>
+          </div>
+
+          <div className="cube h3 w3 l2">
+            <div className="face top"></div>
+            <div className="face left1"></div>
+            <div className="face right1"></div>
+          </div>
+
+          <div className="cube h3 w3 l3">
+            <div className="face top"></div>
+            <div className="face left1"></div>
+            <div className="face right1"></div>
+          </div>
+        </div>
+      </div>
+      <div className="loading-text">Assembling your project</div>
+    </div>
+  );
+};
